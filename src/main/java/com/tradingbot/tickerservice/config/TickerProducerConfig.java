7191f08b100f27db74911303924ddb36090f3195
Frank Park <jehyn923@gmail.com>
2021-09-28 23:18:28 +0900

    feat: Confluent Connection

--- src/main/java/com/tradingbot/tickerservice/config/TickerProducerConfig.java
+++ src/main/java/com/tradingbot/tickerservice/config/TickerProducerConfig.java
@@ -1,6 +1,9 @@
 package com.tradingbot.tickerservice.config;
 
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.config.SaslConfigs;
+import org.apache.kafka.common.config.SslConfigs;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +19,14 @@ import java.util.Map;
 public class TickerProducerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootStrapServers;
+    @Value("${spring.kafka.properties.sasl.jaas.config}")
+    private String JAAS_CONFIG;
+    @Value("${spring.kafka.security.protocol}")
+    private String SECURITY_PROTOCOL;
+    @Value("${spring.kafka.properties.sasl.mechanism}")
+    private String SASL_MECHANISM;
+    @Value("${spring.kafka.ssl.endpoint}")
+    private String SSL_ENDPOINT;
 
     @Bean
     public DirectChannel producerChannel() {
@@ -28,6 +39,10 @@ public class TickerProducerConfig {
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServers);
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG,SECURITY_PROTOCOL);
+        properties.put(SaslConfigs.SASL_JAAS_CONFIG,JAAS_CONFIG);
+        properties.put(SaslConfigs.SASL_MECHANISM,SASL_MECHANISM);
+        properties.put(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, SSL_ENDPOINT);
         properties.put(ProducerConfig.LINGER_MS_CONFIG, 1);
         return properties;
     }
